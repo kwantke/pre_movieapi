f885547cd72d8788487ab22f51566cf3282a4b27
koway <kkwk7310@zpay.or.kr>
2023-01-05 01:10:07 -0800

    add Exception2

--- src/main/java/kr/or/zeropay2/controller/MemberController.java
+++ src/main/java/kr/or/zeropay2/controller/MemberController.java
@@ -15,6 +15,17 @@ public class MemberController {
 
     MemberService memberService;
 
+    /**
+     * 데이터베이스 연결
+     * <pre>
+     * NOTE: 데이터베이스 설정 정보는 application.yml에 설정된
+     *       dataSource 설정 정보이다.
+     *       여기 정보를 가져오는 부분이 @ConfigurationProperties
+     *       이다.
+     * </pre>
+     *
+     * @return
+     */
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
@@ -25,7 +36,12 @@ public class MemberController {
 
     }
 
-
+    /**
+     * EntityManagerFactory 생성
+     *
+     * @param dataSource
+     * @return
+     */
     @GetMapping("/getMember")
     public MemberVo getMember(){
         MemberVo memberVo = memberService.getMemberId("admin");
